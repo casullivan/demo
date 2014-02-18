dae27f5ca5796a943b3cca66a284ff8bb817ccc3
Caleb Sullivan <csullivan@bravecs.com>
2014-02-18 12:48:05 -0500

    auto play

--- assets/site.js
+++ assets/site.js
@@ -2,20 +2,21 @@ $(function() {
   $('#demo1').slidesjs({
     width: 235,
     height: 88,
+    auto: false,
+    restartDelay: 2500,
     navigation: false
   });
 
   $('#demo2').slidesjs({
     width: 235,
     height: 88,
+    auto: false,
+    restartDelay: 2500,
     navigation: false,
     callback:{
     	loaded: function(n){
-    		$(document).bind('touchmove', false);
     	}
     }
   });
-
-  $(document).bind('touchmove', false);
-  //document.ontouchmove = function(e){ e.preventDefault(); }
+  document.ontouchmove = function(e){ e.preventDefault(); }
 });
