950163618ad692c2c24f97ad1b1e4ff62fbfb578
Caleb Sullivan <csullivan@bravecs.com>
2014-02-18 13:17:50 -0500

    force no scroll/no zoom, added wallpaper, css3 rotation

--- assets/site.js
+++ assets/site.js
@@ -2,12 +2,25 @@ $(function() {
   $('#demo1').slidesjs({
     width: 235,
     height: 88,
+    play:{
+	    restartDelay: 500,
+	    auto: true
+	},
     navigation: false
   });
 
   $('#demo2').slidesjs({
     width: 235,
     height: 88,
-    navigation: false
+    play:{
+	    restartDelay: 500,
+	    auto: true
+	},
+    navigation: false,
+    callback:{
+    	loaded: function(n){
+    	}
+    }
   });
+  document.ontouchmove = function(e){ e.preventDefault(); }
 });
--- framework/head.php
+++ framework/head.php
@@ -1,12 +1,16 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
 <html>
  <head>
-  <meta charset="utf-8">
-  <title>DEMO | DEMO</title>
-  <meta name="viewport" content="width=device-width">
-  <link rel="stylesheet" href="/assets/html5reset.css">
-  <link rel="stylesheet" href="/assets/site.css">
+	<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
+	<meta name="apple-mobile-web-app-capable" content="yes">
+	<meta name="format-detection" content="telephone=no">
+
+	<title>DEMO | DEMO</title>
+	<meta name="viewport" content="width=device-width">
+	<link rel="stylesheet" href="/assets/html5reset.css" media="all">
+	<link rel="stylesheet" href="/assets/site.css" media="all">
  </head>
- <body class="zoomContainer">
- <div id = "container" >
+ <body class="zoomViewport">
+ <div id = "container" class="zoomContainer">
 	<?php include("hero.php"); ?>
