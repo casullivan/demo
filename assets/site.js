c0a971b7fd1f278437f3010c294eef700075261c
Caleb Sullivan <csullivan@bravecs.com>
2014-02-18 14:59:17 -0500

    02-18-2014 02:59:17 PM

--- assets/site.js
+++ assets/site.js
@@ -1,27 +1,4 @@
 $(function() {
- //  $('#demo1').slidesjs({
- //    width: 235,
- //    height: 88,
- //    play:{
-	//     restartDelay: 500,
-	//     auto: true
-	// },
- //    navigation: false
- //  });
-
- //  $('#demo2').slidesjs({
- //    width: 235,
- //    height: 88,
- //    play:{
-	//     restartDelay: 500,
-	//     auto: true
-	// },
- //    navigation: false,
- //    callback:{
- //    	loaded: function(n){
- //    	}
- //    }
- //  });
  var mySwiper1 = new Swiper('#demo1',{
     loop:true,
     grabCursor: true,
