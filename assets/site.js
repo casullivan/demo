d0de8f2d8a4a3a0caa036baaf17e7ecc4daf171b
Caleb Sullivan <csullivan@bravecs.com>
2014-02-18 11:16:31 -0500

    rollback

---
+++ assets/site.js
@@ -0,0 +1,13 @@
+$(function() {
+  $('#demo1').slidesjs({
+    width: 235,
+    height: 88,
+    navigation: false
+  });
+
+  $('#demo2').slidesjs({
+    width: 235,
+    height: 88,
+    navigation: false
+  });
+});
--- framework/foot.php
+++ framework/foot.php
@@ -1,3 +1,8 @@
-<div id = "footer"></div>
-</div>
-</body></html>
+	<div id = "footer"></div>
+	</div>
+	<script src="/assets/jquery-1.11.0.js"></script>
+	<script src="/assets/jquery.slides.js"></script>
+	<script src="/assets/jquery.zoomooz.js"></script>
+	<script src="/assets/site.js"></script>
+</body>
+</html>
