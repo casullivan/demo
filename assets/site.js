72444e1a65e37fea3f3aae40b48be694ac609916
Caleb Sullivan <csullivan@bravecs.com>
2014-02-21 16:45:53 -0500

    fixes

--- assets/site.js
+++ assets/site.js
@@ -1,26 +1,27 @@
 $( document ).ready(function() {
-	
+
 	var parentSwiperCount = parseInt($('.parent-swiper').data('count'));
+
 	var swiper1 = new Swiper('.swiper-nested-1',{
-		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeNext(); },
-		onResistanceBefore: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeTo(parentSwiperCount, 100, false); }
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 50 || pixels <= -50 ) parentSwiper.swipeNext(); },
+		onResistanceBefore: function(swiper, pixels){ if(pixels >= 50 || pixels <= -50 ) parentSwiper.swipeTo(parentSwiperCount, 150, false); }
 	});
 	var swiper2 = new Swiper('.swiper-nested-2',{
-		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeNext(); },
-		onResistanceBefore: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipePrev(); }
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 50 || pixels <= -50 ) parentSwiper.swipeNext(); },
+		onResistanceBefore: function(swiper, pixels){ if(pixels >= 50 || pixels <= -50 ) parentSwiper.swipePrev(); }
 	});
 	var swiper3 = new Swiper('.swiper-nested-3',{
-		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeTo(0, 100, false); },
-		onResistanceBefore: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipePrev(); }
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 50 || pixels <= -50 ) parentSwiper.swipeTo(0, 150, false); },
+		onResistanceBefore: function(swiper, pixels){ if(pixels >= 50 || pixels <= -50 ) parentSwiper.swipePrev(); }
 	});
 	var parentSwiper = new Swiper('.parent-swiper',{
 		noSwiping: true,
-		resistance: '100%',
+		resistance: '50%',
 		onResistanceBefore: function(swiper, pixels){ 
-			 if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeTo(parentSwiperCount, 100, false); 
+			 if(pixels >= 50 || pixels <= -50 ) parentSwiper.swipeTo(parentSwiperCount, 150, false); 
 		},
 		onResistanceAfter: function(swiper, pixels){ 
-			 if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeTo(0, 100, false); 
+			 if(pixels >= 50 || pixels <= -50 ) parentSwiper.swipeTo(0, 150, false); 
 		},
 		onSlideChangeStart: function(swiper, direction){
 			swiper1.swipeTo(0);
