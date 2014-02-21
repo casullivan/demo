cf98304f9e76a3f720cc244c62408bc7e1059ee4
Caleb Sullivan <csullivan@bravecs.com>
2014-02-21 16:02:40 -0500

    before switch to linear

--- assets/site.js
+++ assets/site.js
@@ -1,13 +1,26 @@
 $( document ).ready(function() {
 	//$('body').html(window.screen.availHeight+'x'+window.screen.availWidth);
+	var parentSwiperCount = parseInt($('.parent-swiper').data('count'));
+	console.log(parentSwiperCount);
 	var swiper1 = new Swiper('.swiper-nested-1',{
-
-	})
-	var swiper2 = new Swiper('.swiper-nested-2')
-	var swiper3 = new Swiper('.swiper-nested-3')
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 200 || pixels <= -200 ) parentSwiper.swipeNext(); }
+	});
+	var swiper2 = new Swiper('.swiper-nested-2',{
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 200 || pixels <= -200 ) parentSwiper.swipeNext(); }
+	});
+	var swiper3 = new Swiper('.swiper-nested-3',{
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 200 || pixels <= -200 ) parentSwiper.swipeTo(0, 200, false); }
+	});
 	var parentSwiper = new Swiper('.parent-swiper',{
 		mode: 'vertical',
-		onSlideChangeEnd: function(swiper, direction){
+		resistance: '100%',
+		onResistanceBefore: function(swiper, pixels){ 
+			 if(pixels >= 200 || pixels <= -200 ) parentSwiper.swipeTo(parentSwiperCount, 200, false); 
+		},
+		onResistanceAfter: function(swiper, pixels){ 
+			 if(pixels >= 200 || pixels <= -200 ) parentSwiper.swipeTo(0, 200, false); 
+		},
+		onSlideChangeStart: function(swiper, direction){
 			swiper1.swipeTo(0);
 			swiper2.swipeTo(0);
 			swiper3.swipeTo(0);
@@ -16,3 +29,5 @@ $( document ).ready(function() {
 
   document.ontouchmove = function(e){ e.preventDefault(); }
 });
+
+//mySwiper.wrapperTransitionEnd(callback,permanent) 
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php include("framework/head.php"); ?>
 <div class=" parent-swiper swiper-container" data-count="3">
 	<div class="swiper-wrapper">
-		<div class="swiper-slide"><div><?php include("demo1/demo1.php"); ?></div></div>
-		<div class="swiper-slide"><div><?php include("demo2/demo2.php"); ?></div></div>
-		<div class="swiper-slide"><div><?php include("demo3/demo3.php"); ?></div></div>
+		<div class="swiper-slide"><?php include("demo1/demo1.php"); ?></div>
+		<div class="swiper-slide"><?php include("demo2/demo2.php"); ?></div>
+		<div class="swiper-slide"><?php include("demo3/demo3.php"); ?></div>
 	</div>
 </div>
 <?php include("framework/foot.php"); ?>
