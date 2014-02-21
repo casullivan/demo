b4e574f4cb1db97cd886c9e261f4a92be97542d6
Caleb Sullivan <csullivan@bravecs.com>
2014-02-21 16:29:12 -0500

    added external swipe up/down plugin, impoved response, linearly left/right

--- assets/site.js
+++ assets/site.js
@@ -3,13 +3,16 @@ $( document ).ready(function() {
 	var parentSwiperCount = parseInt($('.parent-swiper').data('count'));
 	console.log(parentSwiperCount);
 	var swiper1 = new Swiper('.swiper-nested-1',{
-		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeNext(); }
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeNext(); },
+		onResistanceBefore: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeTo(parentSwiperCount, 100, false); }
 	});
 	var swiper2 = new Swiper('.swiper-nested-2',{
-		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeNext(); }
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeNext(); },
+		onResistanceBefore: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipePrev(); }
 	});
 	var swiper3 = new Swiper('.swiper-nested-3',{
-		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeTo(0, 100, false); }
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeTo(0, 100, false); },
+		onResistanceBefore: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipePrev(); }
 	});
 	var parentSwiper = new Swiper('.parent-swiper',{
 		noSwiping: true,
@@ -26,6 +29,16 @@ $( document ).ready(function() {
 			swiper3.swipeTo(0);
 		}
 	});
+	$(document.body).swipe({
+		swipeUp:function(event, direction, distance, duration) {
+	    	parentSwiper.swipePrev();
+	  	},
+	
+	  	swipeDown:function(event, direction, distance, duration) {
+			parentSwiper.swipeNext();
+	    },
+	});
+		
 
   document.ontouchmove = function(e){ e.preventDefault(); }
 });
--- framework/foot.php
+++ framework/foot.php
@@ -1,7 +1,8 @@
 	<div id = "footer"></div>
 	</div>
-	<script src="/assets/jquery-1.11.0.js"></script>
-	<script src="/assets/swiper/idangerous.swiper.js"></script>
+	<script src="/assets/jquery-1.11.0.js" type="text/javascript"></script>
+	<script src="/assets/swiper/idangerous.swiper.js" type="text/javascript"></script>
+	<script src="/assets/jquery.touchSwipe.js" type="text/javascript"></script>
 	<script src="/assets/site.js"></script>
 </body>
 </html>
