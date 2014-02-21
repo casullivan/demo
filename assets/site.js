0df103fb80176f38af969d1c20dc2421ca6b86fa
Caleb Sullivan <csullivan@bravecs.com>
2014-02-21 16:02:55 -0500

    switched to linear

--- assets/site.js
+++ assets/site.js
@@ -3,22 +3,22 @@ $( document ).ready(function() {
 	var parentSwiperCount = parseInt($('.parent-swiper').data('count'));
 	console.log(parentSwiperCount);
 	var swiper1 = new Swiper('.swiper-nested-1',{
-		onResistanceAfter: function(swiper, pixels){ if(pixels >= 200 || pixels <= -200 ) parentSwiper.swipeNext(); }
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeNext(); }
 	});
 	var swiper2 = new Swiper('.swiper-nested-2',{
-		onResistanceAfter: function(swiper, pixels){ if(pixels >= 200 || pixels <= -200 ) parentSwiper.swipeNext(); }
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeNext(); }
 	});
 	var swiper3 = new Swiper('.swiper-nested-3',{
-		onResistanceAfter: function(swiper, pixels){ if(pixels >= 200 || pixels <= -200 ) parentSwiper.swipeTo(0, 200, false); }
+		onResistanceAfter: function(swiper, pixels){ if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeTo(0, 100, false); }
 	});
 	var parentSwiper = new Swiper('.parent-swiper',{
-		mode: 'vertical',
+		noSwiping: true,
 		resistance: '100%',
 		onResistanceBefore: function(swiper, pixels){ 
-			 if(pixels >= 200 || pixels <= -200 ) parentSwiper.swipeTo(parentSwiperCount, 200, false); 
+			 if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeTo(parentSwiperCount, 100, false); 
 		},
 		onResistanceAfter: function(swiper, pixels){ 
-			 if(pixels >= 200 || pixels <= -200 ) parentSwiper.swipeTo(0, 200, false); 
+			 if(pixels >= 100 || pixels <= -100 ) parentSwiper.swipeTo(0, 100, false); 
 		},
 		onSlideChangeStart: function(swiper, direction){
 			swiper1.swipeTo(0);
