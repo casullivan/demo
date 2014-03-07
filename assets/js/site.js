1718b8272ace6a70d2fdbb8c395bac0fdf5a8151
Caleb Sullivan <csullivan@bravecs.com>
2014-03-07 13:09:53 -0500

    added bootstrap rows

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,29 +1,34 @@
+var $;
 $(document).ready(function() {
 	
-	var S = new Swiper('.parent-swiper',{
+	S = new Swiper('.parent-swiper',{
 		loop: true,
 		autoplay:false,
 		resistance: '100%'
 	});
 
 	S.wrapperTransitionEnd(function(e){
-		if(e.activeSlide().id=='1')
-			$("#video").get(0).play();
+		// if(e.activeSlide().id=='1')
+		// 	$("#video").get(0).play();
 		window.location.hash=e.activeSlide().id;
 	}, true);
 
 
-	var html5Video = function() {
-    return {
-        init: function() {
-            var video = document.getElementsByTagName('video')[0];
-            video.addEventListener('ended', endVideo, false);
-            video.addEventListener('pause', pauseVideo, false);
-            video.addEventListener('play', playVideo, false);
-        }
-    };
-	}();
-	html5Video.init();
+	// var html5Video = function() {
+ //    return {
+ //        init: function() {
+ //            var video = document.getElementsByTagName('video')[0];
+ //            video.addEventListener('ended', endVideo, false);
+ //            video.addEventListener('pause', pauseVideo, false);
+ //            video.addEventListener('play', playVideo, false);
+ //        }
+ //    };
+	// }();
+	// html5Video.init();
+
+	$('a').click(function(){
+		window.location.hash=e.activeSlide().id;
+	});
 
 	function playVideo() { 
 		S.stopAutoplay();
@@ -41,18 +46,18 @@ $(document).ready(function() {
 
 	$(document).swipe({
 	  	swipeDown:function(event, direction, distance, duration) {
-	  		$('#tray').slideDown(400);
+	  		$('#tray').slideDown(100);
 			// S.stopAutoplay();
 			// S.swipeTo(nextBookmark(S.activeIndex), 100);
 	    },
 		swipeUp:function(event, direction, distance, duration) {
-	  		$('#tray').slideUp(400);
+	  		$('#tray').slideUp(100);
 			// S.startAutoplay();
 			// S.swipeTo(prevBookmark(S.activeIndex), 100);
 	  	},
 	});	
 
-	$('.swiper-slide').each(function(i, o) { if($(o).hasClass('b')) addToNav(o); });
+	//$('.swiper-slide').each(function(i, o) { if($(o).attr('id' ,i).hasClass('b')) addToNav(o); });
 });
 
 window.onload = function(){ document.ontouchmove = function(e){ e.preventDefault(); } }
--- slides/demo1/index.php
+++ slides/demo1/index.php
@@ -1,4 +1,32 @@
 
-		<div style="background:url(/images/old_moon.png)" class="swiper-slide b">&nbsp;1/1</div>
+		<div style="background:url(/images/old_moon.png)" class="swiper-slide b">
+				<div class="row-fluid">
+				  <div class="col-md-1">.col-md-1</div>
+				  <div class="col-md-1">.col-md-1</div>
+				  <div class="col-md-1">.col-md-1</div>
+				  <div class="col-md-1">.col-md-1</div>
+				  <div class="col-md-1">.col-md-1</div>
+				  <div class="col-md-1">.col-md-1</div>
+				  <div class="col-md-1">.col-md-1</div>
+				  <div class="col-md-1">.col-md-1</div>
+				  <div class="col-md-1">.col-md-1</div>
+				  <div class="col-md-1">.col-md-1</div>
+				  <div class="col-md-1">.col-md-1</div>
+				  <div class="col-md-1">.col-md-1</div>
+				</div>
+				<div class="row-fluid">
+				  <div class="col-md-8">.col-md-8</div>
+				  <div class="col-md-4">.col-md-4</div>
+				</div>
+				<div class="row-fluid">
+				  <div class="col-md-4">.col-md-4</div>
+				  <div class="col-md-4">.col-md-4</div>
+				  <div class="col-md-4">.col-md-4</div>
+				</div>
+				<div class="row-fluid">
+				  <div class="col-md-6">.col-md-6</div>
+				  <div class="col-md-6">.col-md-6</div>
+				</div>
+			</div>
 		<div style="background:url(/images/1920x1080.gif) no-repeat; " class="swiper-slide">&nbsp;1/2</div>
 		<div style="background-color:orange" class="swiper-slide">1/3</div>
