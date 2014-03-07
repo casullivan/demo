8e4e5e345cf917f9f9b102f1c967d9c065f47e3b
Caleb Sullivan <csullivan@bravecs.com>
2014-03-07 09:47:23 -0500

    added appcache

--- framework/foot.php
+++ framework/foot.php
@@ -1,6 +1,6 @@
 	<div id = "footer"></div>
 	</div>
-	<script src="/assets/jquery-1.11.0.js" type="text/javascript"></script>
+	<script src="/assets/jq/jquery-1.11.0.js" type="text/javascript"></script>
 	<script type="text/javascript">
 	var bookmarks = [];
 	$(document).ready(function(){
@@ -8,8 +8,8 @@
 	});
 	</script>
 	<script src="/assets/swiper/idangerous.swiper.js" type="text/javascript"></script>
-	<script src="/assets/jquery.touchSwipe.js" type="text/javascript"></script>
-	<script src="/assets/site.js" type="text/javascript"></script>
+	<script src="/assets/jq/jquery.touchSwipe.js" type="text/javascript"></script>
+	<script src="/assets/js/site.js" type="text/javascript"></script>
 
 </body>
 </html>
--- framework/head.php
+++ framework/head.php
@@ -1,15 +1,14 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 3.2 Final//EN">
-<html>
+<html manifest="cache.appcache">
  <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 	<meta name="apple-mobile-web-app-capable" content="yes">
 	<meta name="format-detection" content="telephone=no">
-
 	<title>DEMO | DEMO</title>
 	<link rel="stylesheet" href="/assets/swiper/idangerous.swiper.css" media="all">
-	<link rel="stylesheet" href="/assets/html5reset.css" media="all">
-	<link rel="stylesheet" href="/assets/site.css" media="all">
+	<link rel="stylesheet" href="/assets/css/html5reset.css" media="all">
+	<link rel="stylesheet" href="/assets/css/site.css" media="all">
  </head>
  <body>
  <div id = "container">
