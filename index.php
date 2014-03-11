c85b254c66fb0096010d88978f4da4462341578f
Caleb Sullivan <csullivan@bravecs.com>
2014-03-11 08:46:21 -0400

    fixed php alpha order

--- index.php
+++ index.php
@@ -6,10 +6,22 @@ include("framework/head.php"); ?>
 <div class="parent-swiper swiper-container">
 	<div class="swiper-wrapper">
 		<?php
-			$slidesDirectory = new RecursiveDirectoryIterator(realpath('slides'));
-			$Iterator = new RecursiveIteratorIterator($slidesDirectory);
-			$php_files = array_reverse(iterator_to_array(new RegexIterator($Iterator, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH)));
-			foreach ( $php_files as $filename => $file) include($filename);
+
+		class SortIterator extends SplHeap{
+		    public function __construct(Iterator $iterator){
+		        foreach ($iterator as $item) $this->insert($item);
+		    }
+		    public function compare($b,$a){
+		        return strcmp($a->getRealpath(), $b->getRealpath());
+		    }
+		}
+
+		$dit = new RecursiveDirectoryIterator("./slides");
+		$rit = new RecursiveIteratorIterator($dit);
+		$sit = new SortIterator($rit);
+		$files = new RegexIterator($sit, '/^.+\.php$/i', RecursiveRegexIterator::GET_MATCH);
+		foreach ($files as $file) include(implode('|', $file));
+
 		?>
 	</div>
 </div>
