ad0feced439f014077ffa332cb4ec1824f78f1a2
Caleb Sullivan <csullivan@bravecs.com>
2014-03-17 00:12:07 -0400

    fix memory leak

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,10 +1,6 @@
-var S, slide, video, slide_number, autoPlayEnable, holdPosition, autoTimePlay;
+var S, slide, video, slide_number;
 $(document).ready(function() {
 	
-	autoPlayEnable=true;
-	holdPosition=false;
-
-
 	var html5Video = function() {
 	   return {
 	       init: function() {
@@ -20,7 +16,6 @@ $(document).ready(function() {
 		autoplay:false,
 		onSlideChangeStart: function(e, d){
 		slide_number=e.activeLoopIndex;
-		//console.log('slide_number=' + slide_number + '	active index=' + e.activeIndex + '	slide.index=' + $(e).index());
 		
 		if(slide_number==0){
 			$('#tray').slideDown(175);
@@ -31,7 +26,6 @@ $(document).ready(function() {
 
 		if(slide) {
 			video.pause();
-			//S.startAutoplay();
 		}
 
 		if($(e.activeSlide()).hasClass('video')){
@@ -48,9 +42,6 @@ $(document).ready(function() {
 		},
 		resistance: '100%'
 	});
-
-	// S.wrapperTransitionEnd(function(e){
-	// 	}, true);
 
 	if(typeof(Storage)!=="undefined"){
 	  	if (localStorage.getItem('slide_number')){
@@ -74,38 +65,7 @@ $(document).ready(function() {
 	$(document).on('rotate tapthree', function(){
 		window.location.reload();
 	});
-
-	// $(document).on('swipemove', function(){
-	// 	console.log(S.stopAutoplay()); 
-		
-	// 	console.log('swipe');
-	// 	pauseAutoplay(S,14000);
-	// });
-
-
-	// $(document).on('swiperight', function(){
-	// 	console.log('swiperight');
-	// 	pauseAutoplay(S, 2000);
-	// 	S.swipeNext();
-	// });
-
-	// $(document).on('swipeleft', function(){
-	// 	console.log('left');
-	// 	pauseAutoplay(S, 2000);
-	// 	S.swipePrev();
-	// });	
-
-	// tap local
-	$('t').on('tapone', function(e){
-		console.log($(this).data('go'));
-		goTo($(this).data('go'));
-	});
-
-	// tap href
-	$('tap').on('tapone', function(e){
-		window.location+=$(this).attr('href');
-	});
-
+	
 	$(".pause_play").on('tapone', function(){
 		if(slide){
 			if($(this).hasClass('play'))
@@ -127,12 +87,10 @@ $(document).ready(function() {
 	function endVideo() { 
 		$(".pause_play").removeClass('play');
 		if(slide) video.currentTime=0;
-		//S.startAutoplay();
 	}
 
 	function pauseVideo() { 
 		$(".pause_play").removeClass('play');
-		//pauseAutoplay(S, 5000);
 	}
 
 	$(document).swipe({
@@ -145,17 +103,6 @@ $(document).ready(function() {
 	});	
 });
 
-function pauseAutoplay(swiper, duration){
-	// console.log("pauseAutoPlay");
-	// swiper.stopAutoplay();
-	// window.clearTimeout(autoTimePlay);
-	// autoTimePlay = window.setTimeout(function(){ swiper.startAutoplay(); }, duration);
-}
-
 function goTo(selector){
-	//pauseAutoplay(S, 4000);
-	//console.log(selector + ' ' + $(selector).index() + ' ' + S.swipeTo(($(selector).index()-1), 500));
-	S.swipeTo(($(selector).index()-1), 500);
+	S.swipeTo(($(selector).index()-1), 1000);
 }
-
-//window.onload = function(){ document.ontouchmove = function(e){ e.preventDefault(); } }
